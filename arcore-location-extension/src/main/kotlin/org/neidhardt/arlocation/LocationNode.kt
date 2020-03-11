d7227a7060a9556385736510b6e9fc2808524cbc
Eric Neidhardt <eric.neidhardt@gmail.com>
2020-03-11 10:56:48 +0100

    Finished cleanup

--- arcore-location-extension/src/main/kotlin/org/neidhardt/arlocation/LocationNode.kt
+++ arcore-location-extension/src/main/kotlin/org/neidhardt/arlocation/LocationNode.kt
@@ -18,7 +18,7 @@ import kotlin.math.sqrt
 class LocationNode(
 		anchor: Anchor,
 		val locationMarker: LocationMarker,
-		val locationScene: CustomLocationScene
+		val locationScene: LocationScene
 ) : AnchorNode(anchor) {
 
 	var renderEventListener: RenderEventListener? = null
@@ -48,13 +48,13 @@ class LocationNode(
 
 				// Compute the straight-line distance.
 				distanceInAR = sqrt(dx * dx + dy * dy + (dz * dz).toDouble())
-				if (locationScene.shouldOffsetOverlapping()) {
-					if (locationScene.mArSceneView.scene.overlapTestAll(n).size > 0) {
+				if (locationScene.offsetOverlapping) {
+					if (locationScene.arSceneView.scene.overlapTestAll(n).size > 0) {
 						height += 1.2f
 					}
 				}
 
-				if (locationScene.shouldRemoveOverlapping()) {
+				if (locationScene.removeOverlapping) {
 					val ray = Ray()
 					ray.origin = cameraPosition
 					val xDelta = (distanceInAR * sin(Math.PI / 15)).toFloat() //12 degrees
@@ -69,7 +69,7 @@ class LocationNode(
 			}
 		}
 
-		if (!locationScene.minimalRefreshing()) {
+		if (!locationScene.minimalRefreshing) {
 			scaleAndRotate()
 		}
 
@@ -88,7 +88,7 @@ class LocationNode(
 	): Boolean {
 		val nodeDirection = Vector3.subtract(target, cameraPosition)
 		ray.direction = nodeDirection
-		val hitTestResults = locationScene.mArSceneView.scene.hitTestAll(ray)
+		val hitTestResults = locationScene.arSceneView.scene.hitTestAll(ray)
 		if (hitTestResults.size > 0) {
 
 			var closestHit: HitTestResult? = null
@@ -106,56 +106,57 @@ class LocationNode(
 	}
 
 	fun scaleAndRotate() {
+
+		val location = locationScene.location ?: return
+
 		scene?.let { scene ->
-			locationScene.deviceLocation?.currentBestLocation?.let { deviceLocation ->
-				children.forEach { n ->
-					val markerDistance = ceil(
-							LocationUtils.distance(
-									locationMarker.latitude,
-									deviceLocation.latitude,
-									locationMarker.longitude,
-									deviceLocation.longitude,
-									0.0, 0.0
-							)
-					).toInt()
-					distance = markerDistance
+			children.forEach { n ->
+				val markerDistance = ceil(
+						LocationUtils.distance(
+								locationMarker.latitude,
+								location.latitude,
+								locationMarker.longitude,
+								location.longitude,
+								0.0, 0.0
+						)
+				).toInt()
+				distance = markerDistance
 
-					// limit the distance of the Anchor within the scene.
-					var renderDistance = markerDistance
-					if (renderDistance > locationScene.distanceLimit) renderDistance = locationScene.distanceLimit
-					val cameraPosition = scene.camera.worldPosition
-					val direction = Vector3.subtract(cameraPosition, n.worldPosition)
+				// limit the distance of the Anchor within the scene.
+				var renderDistance = markerDistance
+				if (renderDistance > locationScene.distanceLimit) renderDistance = locationScene.distanceLimit
+				val cameraPosition = scene.camera.worldPosition
+				val direction = Vector3.subtract(cameraPosition, n.worldPosition)
 
-					var scale = 1f
-					when (scalingMode) {
-						LocationMarker.ScalingMode.FIXED_SIZE_ON_SCREEN -> {
-							scale =
-									sqrt(direction.x * direction.x + direction.y * direction.y + (direction.z * direction.z).toDouble()).toFloat()
-						}
-						LocationMarker.ScalingMode.GRADUAL_TO_MAX_RENDER_DISTANCE -> {
-							val scaleDifference =
-									gradualScalingMaxScale - gradualScalingMinScale
-							scale =
-									(gradualScalingMinScale + (locationScene.distanceLimit - markerDistance) * (scaleDifference / locationScene.distanceLimit)) * renderDistance
-						}
-						LocationMarker.ScalingMode.GRADUAL_FIXED_SIZE -> {
-							scale =
-									sqrt(direction.x * direction.x + direction.y * direction.y + (direction.z * direction.z).toDouble()).toFloat()
-							var gradualScale = gradualScalingMaxScale - gradualScalingMinScale
-							gradualScale = gradualScalingMaxScale - gradualScale / renderDistance * markerDistance
-							scale *= gradualScale.coerceAtLeast(gradualScalingMinScale)
-						}
-						LocationMarker.ScalingMode.NO_SCALING -> {
-							scale = 1f
-						}
+				var scale = 1f
+				when (scalingMode) {
+					LocationMarker.ScalingMode.FIXED_SIZE_ON_SCREEN -> {
+						scale =
+								sqrt(direction.x * direction.x + direction.y * direction.y + (direction.z * direction.z).toDouble()).toFloat()
 					}
-
-					scale *= scaleModifier
-					n.worldPosition = Vector3(n.worldPosition.x, height, n.worldPosition.z)
-					val lookRotation = Quaternion.lookRotation(direction, Vector3.up())
-					n.worldRotation = lookRotation
-					n.worldScale = Vector3(scale, scale, scale)
+					LocationMarker.ScalingMode.GRADUAL_TO_MAX_RENDER_DISTANCE -> {
+						val scaleDifference =
+								gradualScalingMaxScale - gradualScalingMinScale
+						scale =
+								(gradualScalingMinScale + (locationScene.distanceLimit - markerDistance) * (scaleDifference / locationScene.distanceLimit)) * renderDistance
+					}
+					LocationMarker.ScalingMode.GRADUAL_FIXED_SIZE -> {
+						scale =
+								sqrt(direction.x * direction.x + direction.y * direction.y + (direction.z * direction.z).toDouble()).toFloat()
+						var gradualScale = gradualScalingMaxScale - gradualScalingMinScale
+						gradualScale = gradualScalingMaxScale - gradualScale / renderDistance * markerDistance
+						scale *= gradualScale.coerceAtLeast(gradualScalingMinScale)
+					}
+					LocationMarker.ScalingMode.NO_SCALING -> {
+						scale = 1f
+					}
 				}
+
+				scale *= scaleModifier
+				n.worldPosition = Vector3(n.worldPosition.x, height, n.worldPosition.z)
+				val lookRotation = Quaternion.lookRotation(direction, Vector3.up())
+				n.worldRotation = lookRotation
+				n.worldScale = Vector3(scale, scale, scale)
 			}
 		}
 	}
